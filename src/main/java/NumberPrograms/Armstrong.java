f6de3fd33b61689f702f03ec1967d7deca7aee54
Shitalbhosale11 <shitalb075@gmail.com>
2026-02-07 17:20:54 +0530

    added 1 program

--- src/main/java/NumberPrograms/Armstrong.java
+++ src/main/java/NumberPrograms/Armstrong.java
@@ -1,4 +1,22 @@
-package NumberPrograms;
+import java.util.Scanner;
 
-public class Armstrong {
+class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int temp = num, sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num = num / 10;
+        }
+
+        if (sum == temp)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not an Armstrong Number");
+    }
 }

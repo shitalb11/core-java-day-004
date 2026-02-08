f7d80302c93efd2b73ec12f5afc373ab255c62cc
Shitalbhosale11 <shitalb075@gmail.com>
2026-02-08 22:15:16 +0530

    added 1 program

---
+++ src/main/java/NumberPrograms/StrongNumber.java
@@ -0,0 +1,30 @@
+package NumberPrograms;
+
+import java.util.Scanner;
+
+class StrongNumber {
+    static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++)
+            fact *= i;
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int temp = num, sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += factorial(digit);
+            num = num / 10;
+        }
+
+        if (sum == temp)
+            System.out.println("Strong Number");
+        else
+            System.out.println("Not a Strong Number");
+    }
+}
